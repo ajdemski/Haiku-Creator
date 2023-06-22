d67d5cb353b454d4cc5abaaf77863f6b89c653f8
Aaron Demski <ajdemski@frontier.com>
2023-06-22 11:16:12 -0700

    good fail on constructor test

---
+++ __tests__/haikuChecker.test.js
@@ -0,0 +1,31 @@
+/* import Triangle from './../src/triangle.js';
+
+describe('Triangle', () => {
+
+  test('should correctly create a triangle object with three lengths', () => {
+    const triangle = new Triangle(2,4,5);
+    expect(triangle.side1).toEqual(2);
+    expect(triangle.side2).toEqual(4);
+    expect(triangle.side3).toEqual(5);
+  });
+}); */
+
+let haiku; import HaikuChecker from './../src/haikuChecker.js';
+
+
+describe('HaikuChecker', () => {
+  let haiku;
+
+  beforeEach(() => {
+    haiku = `A world of dew,
+
+        And within every dewdrop
+        
+        A world of struggle.`;
+  });
+
+  test('should define HaikuChecker constructor', () => {
+    let haikuChecker = new HaikuChecker(haiku);
+    expect(haikuChecker(haiku)).toBe(haiku);
+  });
+});
